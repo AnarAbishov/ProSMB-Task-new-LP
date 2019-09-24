5f29c0f7d552e8d52357369269f9b246f4530299
AnarAbishov <aanar.p303@code.edu.az>
2019-09-24 23:04:51 +0400

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,117 @@
+$( document ).ready(function() {
+    
+
+
+$('.trust .owl-carousel').owlCarousel({
+  autoplay: true,
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  nav:false,
+  dot:false,
+ 
+})
+
+$('.feature .owl-carousel').owlCarousel({
+    items:3,
+    loop:true,
+    nav:true,
+    center:true,
+  });
+
+  $('.pricing .owl-carousel').owlCarousel({
+    autoplay: true,
+    items:1.5,
+    loop:true,
+    center:true,
+    dot:true,
+  });
+
+
+
+  let owl = $(".feature .owl-carousel")
+  owl.on('change.owl.carousel', function(e){
+    console.log("fvsv")
+    var value= $(".active.center .item-img").attr('id');
+    var currentActive=$(`.feature .text .text-data[data-value=${value}]`);
+    $(".text-data").removeClass("active-text");
+    currentActive.addClass("active-text");
+
+    })
+
+
+ $('.owl-next').click(function() {
+  console.log("fvsv")
+  var value= $(".active.center").find(".item-img").attr('id');
+  var currentActive=$(`.feature .text .text-data[data-value=${value}]`);
+  $(".text-data").removeClass("active-text");
+  currentActive.addClass("active-text");
+
+})
+$('.owl-prev').click(function() {
+  console.log("fvsv")
+  var value= $(".active.center").find(".item-img").attr('id');
+  var currentActive=$(`.feature .text .text-data[data-value=${value}]`);
+  $(".text-data").removeClass("active-text");
+  currentActive.addClass("active-text");
+
+})
+
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+    })
+    
+
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 40) {
+            $('.head').addClass('fixed-header');
+            $(".logo-mob img").attr("src","img/logo.png")
+        }
+        else {
+            $('.head').removeClass('fixed-header');
+            $(".logo-mob img").attr("src","img/logo-black.svg")
+
+        }
+    });
+
+    $("#nav #nav-items ul li a").click(function(){
+      $("#nav").removeClass('active');
+    })
+
+});
+
+jQuery(document).ready(function ($) {
+    $('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+         {var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({scrollTop: target.offset().top-100}, 1000);return false;}
+            }
+    });
+})
+
+
+function newFunction(owl) {
+  owl();
+}
+
+function main() {
+    var nav = document.getElementById('nav');
+    nav.addEventListener('click',function() {
+      nav.className = (!nav.classList.contains('active')) ? 'active' : '';
+
+    });
+  }
+  addEventListener('load',main);
+
+  AOS.init();
+
+ 
